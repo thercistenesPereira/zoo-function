09a5b4334209d37794eaf97caeb51ab363460a5e
Thercístenes <thercistenes1@outlook.com>
2023-06-15 16:03:21 -0300

    8. Implemente a função calculateEntr

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,48 +1,7 @@
 const data = require('../data/zoo_data');
 
-const getEmployeesCoverage = (options = {}) => {
-  const { id, name } = options;
-  const employees = data.employees;
-
-  if (id) {
-    const employee = employees.find((emp) => emp.id === id);
-    if (!employee) {
-      throw new Error('Informações inválidas');
-    }
-    const { firstName, lastName, responsibleFor } = employee;
-    const fullName = `${firstName} ${lastName}`;
-    const animalNames = responsibleFor.map((animalId) =>
-      data.animals.find((animal) => animal.id === animalId).name
-    );
-    return { [fullName]: animalNames };
-  }
-
-  if (name) {
-    const employee = employees.find(
-      (emp) => emp.firstName === name || emp.lastName === name
-    );
-    if (!employee) {
-      throw new Error('Informações inválidas');
-    }
-    const { firstName, lastName, responsibleFor } = employee;
-    const fullName = `${firstName} ${lastName}`;
-    const animalNames = responsibleFor.map((animalId) =>
-      data.animals.find((animal) => animal.id === animalId).name
-    );
-    return { [fullName]: animalNames };
-  }
-
-  const coverage = {};
-  employees.forEach((employee) => {
-    const { firstName, lastName, responsibleFor } = employee;
-    const fullName = `${firstName} ${lastName}`;
-    const animalNames = responsibleFor.map((animalId) =>
-      data.animals.find((animal) => animal.id === animalId).name
-    );
-    coverage[fullName] = animalNames;
-  });
-
-  return coverage;
+const getEmployeesCoverage = () => {
+  // seu código aqui.
 };
 
 module.exports = getEmployeesCoverage;
