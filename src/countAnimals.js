f8eafffc68744c6e58015cf8a871fdad7a467195
Thercístenes <thercistenes1@outlook.com>
2023-06-15 00:07:29 -0300

    5. Implemente a função countAnimals.

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,27 @@
 const data = require('../data/zoo_data');
 
+const countAllSpecies = () => data.species.reduce((acc, specie) => {
+  acc[specie.name] = specie.residents.length;
+  return acc;
+}, {});
+
+const findSpecies = (speciesName) => data.species.find((s) => s.name === speciesName);
+
+const countAllResidents = (species) => species.residents.length;
+
+const countResidentsBySex = (species, sex) => species
+  .residents.filter((resident) => resident.sex === sex).length;
+
 const countAnimals = (animal) => {
-  // seu código aqui
+  if (!animal) {
+    return countAllSpecies();
+  }
+  if (findSpecies(animal.species)) {
+    if (!animal.sex) {
+      return countAllResidents(findSpecies(animal.species));
+    }
+    return countResidentsBySex(findSpecies(animal.species), animal.sex);
+  }
 };
 
 module.exports = countAnimals;
--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,17 @@
 const data = require('../data/zoo_data');
 
 const isManager = (id) => {
-  // seu código aqui
+  // const { employees } = data;
+  // return employees.some((employee) => id === employee.id && employee.managers > 0);
 };
 
 const getRelatedEmployees = (managerId) => {
-  // seu código aqui
+  // if (isManager(managerId)) {
+  //   throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  // }
+  // const { employees } = data;
+  // return employees.filter((employee) => employee.managers.includes(managerId))
+  //   .map((employee) => `${employee.firstName} ${employee.lastName}`);
 };
 
 module.exports = { isManager, getRelatedEmployees };
