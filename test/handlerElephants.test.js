48579ddcabe27e7d147e6d2411d79b7577bbf7d6
Thercístenes <thercistenes1@outlook.com>
2023-06-15 07:52:44 -0300

    6. Obtenha ao menos 80% de cobertura de testes na função handlerElephants.

--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -1,3 +1,36 @@
 const handlerElephants = require('../src/handlerElephants');
 
-describe('Testes da função HandlerElephants', () => {});
+describe('Testes da função handlerElephants', () => {
+  it('Deve retornar a quantidade de elefantes', () => {
+    const result = handlerElephants('count');
+    expect(result).toBe(4);
+  });
+  it('Deve retornar um array com os nomes dos elefantes', () => {
+    const result = handlerElephants('names');
+    expect(result).toEqual(expect.arrayContaining(['Ilana', 'Orval', 'Bea', 'Jefferson']));
+  });
+
+  it('Deve retornar a média de idade dos elefantes', () => {
+    const result = handlerElephants('averageAge');
+    expect(result).toBeCloseTo(10.5);
+  });
+
+  it('Deve retornar a localização dos elefantes', () => {
+    const result = handlerElephants('location');
+    expect(result).toBe('NW');
+  });
+
+  it('Deve retornar a popularidade dos elefantes', () => {
+    const result = handlerElephants('popularity');
+    expect(result).toBe(5);
+  });
+  it('Deve retornar os dias de disponibilidade dos elefantes', () => {
+    const result = handlerElephants('availability');
+    expect(result).toEqual(expect.arrayContaining(['Friday', 'Saturday', 'Sunday', 'Tuesday']));
+  });
+
+  it('Deve retornar null para um parâmetro inválido', () => {
+    const result = handlerElephants('invalid');
+    expect(result).toBeNull();
+  });
+});
