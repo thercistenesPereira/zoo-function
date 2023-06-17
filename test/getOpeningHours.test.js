0291b63f6563efec1adafcf900eef6e73b65becb
Thercístenes <thercistenes1@outlook.com>
2023-06-17 15:45:11 -0300

    12. Obtenha ao menos 85% de cobertura de testes na função getOpeningHours.

--- test/getOpeningHours.test.js
+++ test/getOpeningHours.test.js
@@ -1,3 +1,31 @@
 const getOpeningHours = require('../src/getOpeningHours');
 
-describe('Testes da função getOpeningHours', () => {});
+describe('Testes da função getOpeningHours', () => {
+  it('A função getOpeningHours está definida', () => {
+    expect(getOpeningHours).toBeDefined();
+  });
+
+  it('getOpeningHours é uma função', () => {
+    expect(typeof getOpeningHours).toEqual('function');
+  });
+
+  it('A função getOpeningHours  retorna {Tuesday: { open: 8, close: 6 }, Wednesday: { open: 8, close: 6 },Thursday: { open: 10, close: 8 }, Friday: { open: 10, close: 8 }, Saturday: { open: 8, close: 10 },Sunday: { open: 8, close: 8 }, Monday: { open: 0, close: 0 }}', () => {
+    expect(getOpeningHours()).toEqual({
+      Tuesday: { open: 8, close: 6 },
+      Wednesday: { open: 8, close: 6 },
+      Thursday: { open: 10, close: 8 },
+      Friday: { open: 10, close: 8 },
+      Saturday: { open: 8, close: 10 },
+      Sunday: { open: 8, close: 8 },
+      Monday: { open: 0, close: 0 },
+    });
+  });
+
+  it('Ao receber os argumentos "Monday", "09:00-AM" retorna The zoo is closed', () => {
+    expect(getOpeningHours('Monday', '09:00-AM')).toEqual('The zoo is closed');
+  });
+
+  it('Ao receber os argumentos "Tuesday", "09:00-AM" retorna The zoo is closed', () => {
+    expect(getOpeningHours('Tuesday', '09:00-AM')).toEqual('The zoo is open');
+  });
+});
